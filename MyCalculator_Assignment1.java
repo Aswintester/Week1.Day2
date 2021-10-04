cca1e6e04e52e45c23d4dec3df386f58fe8eeabd
Aswintester <55643261+Aswintester@users.noreply.github.com>
2021-10-04 23:59:27 +0530

    Add files via upload

---
+++ MyCalculator_Assignment1.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class MyCalculator_Assignment1 {
+
+	public static void main(String[] args) {
+		Calculator_Assignment1 obj  = new Calculator_Assignment1();
+		System.out.println("The sum is : "+ obj.add());
+		System.out.println("The mul is : "+ obj.mul());
+		System.out.println("The Div is : "+ obj.div());
+		System.out.println("The sub is : "+ obj.sub());
+		
+	}
+
+}
